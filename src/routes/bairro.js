a4c8cb3da157d330e30f9bae3a194e1f152043e8
Jose Manuel Jr <josemanuelsimangojunior@gmail.com>
2023-01-06 11:47:30 +0200

    provincia done kinda?

--- src/routes/bairro.js
+++ src/routes/bairro.js
@@ -8,4 +8,23 @@ router.get('/',async(req,res)=>{
     res.send(list)
 })
 
+router.get('/:provincia',async(req,res)=>{
+    const snapshot = await Bairros.get()
+    const list = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
+    const provincia = req.params.provincia.toLowerCase();
+    //console.log(stringify(provincia))
+    var result = list.filter(obj => {
+        var id = obj.id.toLowerCase();
+        id = id.replace(" ",'_')
+        console.log(id)
+        
+        return id == provincia
+    })
+    
+    res.send(result)
+    // res.send(list[1]["id"])
+})
+
+
+
 module.exports = router
